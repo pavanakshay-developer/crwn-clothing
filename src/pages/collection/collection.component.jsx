68e0ca416cd4b44bcc01aab8bb8ff9f7a368bcf8
pavanakshay <pavanakshay.developer@gmail.com>
2021-07-30 17:08:22 -0500

    changed the shop data using objects instead of arrays for better performance. added new reducers for collections. Showing collection based on url params

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
+const CollectionPage = ({ collection: { title, items } }) => (
   <div className="collection-page">
-    <h2>COLLECTION PAGE</h2>
+    <h2>{title}</h2>
+    <div className="items">
+      {items.map((item) => (
+        <CollectionItem key={item.id} item={item} />
+      ))}
+    </div>
   </div>
 );
 
